25621a0c311c6136d2f5e18cc0a91846d1f8d761
hoxton314 <igorkalicinski@gmail.com>
2021-12-09 11:24:05 +0100

    bugfix

--- components/NoteList.jsx
+++ components/NoteList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, TouchableOpacity } from 'react-native'
+import { Text, StyleSheet, View, TouchableOpacity, Alert } from 'react-native'
 import * as SecureStore from 'expo-secure-store';
 import { FlatList } from 'react-native-gesture-handler';
 import { useIsFocused } from '@react-navigation/native';
@@ -16,14 +16,29 @@ export default class NoteList extends Component {
         super(props)
         this.state = {
             key: this.props.route.params.key ? this.props.route.params.key : 123,
-            colors: ['#CCD4BF', '#E7CBA9', '#EEBAB2', '#F5F3E7', '#F5E2E4'],
+            colors: ['#EF476F', '#FFD166', '#06D6A0', '#118AB2', '#073B4C'],
             data: [{ title: '123', text: '2rqwfgsg', date: '23 Dec' }, { title: '123', text: '2rqwfgsg', date: '23 Dec' }, { title: '123', text: '2rqwfgsg', date: '23 Dec' }]
         }
     }
-    componentDidUpdate(prevProps, prevState) {
+    async componentDidUpdate(prevProps, prevState) {
         if (prevProps.route.params.key != this.props.route.params.key) {
+            let keys = (await this.getItem("keys")).split(',')
+            console.log(keys)
+            let data = []
+            keys.forEach(async (key) => {
+                let toPush = await this.getItem(key)
+                toPush = JSON.parse(toPush)
+                data.push(toPush)
+            })
+            while (data.length != keys.length) {
+                await new Promise(resolve => setTimeout(resolve, 100));
+            }
+            console.log(data)
+            this.setState({ data: data })
+
+
             this.setState({ key: this.props.route.params.key })
-            console.log('key upd')
+            console.log('================key update================', this.props.route.params.key)
         }
     }
     async componentDidMount() {
@@ -51,12 +66,26 @@ export default class NoteList extends Component {
         let result = await SecureStore.getItemAsync(key);
         return result
     }
+    delAlert() {
+        Alert.alert(
+            "Czy chcesz usunąć?",
+            "Nie będzie możliwości przywrócenia danych!",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { text: "OK", onPress: this.delete() }
+            ]
+        )
+    }
     render() {
 
         const renderItem = ({ item }) => (
-            <TouchableOpacity onLongPress={() => { console.log('delete') }} style={{ padding: 15, borderRadius: 20, marginTop: 10, flex: 1 / 2.2, height: 150, backgroundColor: this.state.colors[Math.floor(Math.random() * (5))] }}>
+            <TouchableOpacity onLongPress={this.delAlert.bind(this)} style={{ padding: 15, borderRadius: 20, marginTop: 10, flex: 1 / 2.2, height: 150, backgroundColor: this.state.colors[Math.floor(Math.random() * (5))] }}>
 
-                <View style={{ flexDirection: 'row' }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 5 }}>
                     <Text style={styles.title}>{item.title}</Text>
                     <Text style={styles.title}>{item.date}</Text>
                 </View>
@@ -74,7 +103,7 @@ export default class NoteList extends Component {
                     columnWrapperStyle={{ justifyContent: 'space-evenly' }}
                     data={this.state.data}
                     renderItem={renderItem}
-                    keyExtractor={item => item.id}
+                    keyExtractor={item => Math.floor(Math.random() * (99999999999999))}
                     style={styles.flatlist}
                     numColumns={2}
                     key={this.state.key}
